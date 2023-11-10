743a56714fbd54968e8fb8469814be5129afaaa7
GUI <guixiaokang080905@163.com>
2023-11-10 22:23:05 +0800

    push

--- script/main.js
+++ script/main.js
@@ -115,7 +115,7 @@ const animationTimeline = () => {
       0.05
     )
     .to(".fake-btn", 0.1, {
-      backgroundColor: "rgb(127, 206, 248)"
+      backgroundColor: "rgb(5, 193, 96)"
     })
     .to(
       ".four",
